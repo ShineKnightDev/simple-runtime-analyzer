5d271f59d00db1b1c72f5301716fe99bb1712367
ShineKnightDev <diegoosoriovzla@gmail.com>
2025-10-04 19:43:17 -0400

    fix: header.html

---
+++ index.js
@@ -0,0 +1,21 @@
+var index =
+[
+    [ "Overview", "index.html#autotoc_md1", [
+      [ "Motivation", "index.html#autotoc_md3", null ],
+      [ "Features", "index.html#autotoc_md5", null ],
+      [ "Library Modules", "index.html#autotoc_md7", null ],
+      [ "Output Formats", "index.html#autotoc_md9", null ],
+      [ "Examples", "index.html#autotoc_md11", null ],
+      [ "Getting Started", "index.html#autotoc_md13", [
+        [ "Requirements", "index.html#autotoc_md14", null ],
+        [ "Installation", "index.html#autotoc_md15", [
+          [ "C++ Library Usage", "index.html#autotoc_md16", null ],
+          [ "Plotting Script Usage", "index.html#autotoc_md17", null ]
+        ] ]
+      ] ],
+      [ "Project Tree", "index.html#autotoc_md19", null ],
+      [ "Building the Project", "index.html#autotoc_md21", null ],
+      [ "Documentation", "index.html#autotoc_md23", null ],
+      [ "Community & Contributing", "index.html#autotoc_md25", null ]
+    ] ]
+];
